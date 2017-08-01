275b7c9549a8cc3872ab944c6396fc960c321e66
MrElektronix <milovanpelt@gmail.com>
2017-08-01 13:58:19 +0200

    update

---
+++ Assets/Coroutine.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coroutine : MonoBehaviour {
+
+
+	[SerializeField] float timerTime;
+	// Use this for initialization
+
+	public void StopTimer() {
+		StopCoroutine ("RunTimer");
+	}
+
+	public void StartTimer() {
+		StopTimer ();
+		StartCoroutine ("RunTimer");
+	}
+
+	public void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator RunTimer() {
+		float timer = 0;
+		while (timer < timerTime) {
+			timer += Time.deltaTime;
+			Debug.Log (timer);
+
+			yield return null;
+		}
+			
+		Debug.Log ("einde");
+	}
+}
